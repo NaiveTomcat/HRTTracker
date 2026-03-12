5e42b59cedf38658a2f96439e5d5702f6b07bef3
NaiveTomcat <tomdang@tsinghuamakerxian.cn>
2026-03-12 14:16:26 +0800

    chore: bump version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
         applicationId = "cn.naivetomcat.hrt_tracker"
         minSdk = 31
         targetSdk = 36
-        versionCode = 10050
+        versionCode = 10060
         versionName = "git describe --tags --always --dirty=-dev".runCommand(workingDir = rootDir)
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
